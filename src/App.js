02284f3da30d6239b9b862db7fbedf05e5f44830
Mohammad Anas <0zns20@gmail.com>
2022-12-24 23:48:11 +0530

    Added About page and modified Contact css and Footer css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
 import Contact from "./Components/Contact";
+import About from "./Components/About";
 
 function App() {
   return (
     <div className="App">
       {/* <h1>Creating my Portfolio. Hope I can complete this and start freelancing to earn more bucks.</h1> */}
       <Navbar />
-      <div id="div">
-        <Contact />
-      </div>
+      <About />
+      <Contact />
       <Footer />
     </div>
   );
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import Resume from './../assets/about.jpg';
 
 const Navbar = () => {
     return (<>
-        <nav className="navbar navbar-expand-lg mr-auto">
+        <nav className="navbar navbar-expand-lg navbar-light mr-auto sticky-top">
             <div className="container-fluid">
                 <a className="navbar-brand" href="/">Mohammad Anas</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -12,13 +13,10 @@ const Navbar = () => {
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="/">Home</a>
-                            </li>
-                            <li className="nav-item">
                                 <a className="nav-link" href="/">Project</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="/">Resume</a>
+                                <a className="nav-link" href={Resume} download="Resume.jpg" rel='noreferrer' target='_blank'>Resume</a>
                             </li>
                             <li className="nav-item">
                                 <a className="nav-link" href="/">About Me</a>
