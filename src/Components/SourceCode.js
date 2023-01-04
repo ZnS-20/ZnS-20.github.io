9bf32788e40e7519a19c97b83021af3364997ff5
Mohammad Anas <0zns20@gmail.com>
2023-01-05 00:51:43 +0530

    Configure React app for deployment to Github Pages

--- src/Components/SourceCode.js
+++ src/Components/SourceCode.js
@@ -14,7 +14,9 @@ const SourceCode = () => {
                     <p className='centerContent'>
                         The Website is insiped by <a id='link' href='https://lokkeestudios.com/' target='_blank' rel="noreferrer">"LOOKEE STUDIOS"</a>. If you want to explore the code of this website you can click on 'Explore Code' button.
                     </p>
-                    <button className='btn btn-primary' id='explorebtn'>Explore Code</button>
+                    <a href='https://github.com/ZnS-20/zns-20.github.io' target='_blank' rel="noreferrer">
+                        <button className='btn btn-primary' id='explorebtn'>Explore Code</button>
+                    </a>
                 </div>
             </div>
         </div>
