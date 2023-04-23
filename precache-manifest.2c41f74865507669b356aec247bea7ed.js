fb8b8e524142721a94d4780379b67400314d4e16
Mohammad Anas <0zns20@gmail.com>
2023-04-24 01:17:20 +0530

    Updates

---
+++ precache-manifest.2c41f74865507669b356aec247bea7ed.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "129d7726547d859ae92f",
+    "url": "/static/css/main.8ec4ce69.chunk.css"
+  },
+  {
+    "revision": "129d7726547d859ae92f",
+    "url": "/static/js/main.129d7726.chunk.js"
+  },
+  {
+    "revision": "e276198e4f4e1ff9cafe",
+    "url": "/static/js/1.e276198e.chunk.js"
+  },
+  {
+    "revision": "d6667061b682f522423a",
+    "url": "/static/css/2.cd830982.chunk.css"
+  },
+  {
+    "revision": "d6667061b682f522423a",
+    "url": "/static/js/2.d6667061.chunk.js"
+  },
+  {
+    "revision": "54ef21401d50fd971d2b",
+    "url": "/static/js/runtime~main.54ef2140.js"
+  },
+  {
+    "revision": "916c09b3f147ba82139c8d6db1cff250",
+    "url": "/static/media/Mohammad_Anas_Resume.916c09b3.pdf"
+  },
+  {
+    "revision": "6ae7fd10869911668ca4f2f966a53d9e",
+    "url": "/static/media/about.6ae7fd10.jpg"
+  },
+  {
+    "revision": "b015073190c1b30125bc7078a150ead6",
+    "url": "/static/media/source1.b0150731.jpg"
+  },
+  {
+    "revision": "f0c3424326a629e6a74326b0988d9e1b",
+    "url": "/static/media/image1.f0c34243.png"
+  },
+  {
+    "revision": "2e61b54c72bdc261e675dd81db50e923",
+    "url": "/static/media/image2.2e61b54c.jpg"
+  },
+  {
+    "revision": "8cbfb9e3f9fc2b5dcba54b09176e053b",
+    "url": "/static/media/Home.8cbfb9e3.png"
+  },
+  {
+    "revision": "a894156019961961abab2f499f0ef741",
+    "url": "/index.html"
+  }
+];
